89b415e66db367774ed4eeb9c37fd52c00ff26c2
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-12-01 11:18:57 -0300

    criados Getter e Setters.

--- src/main/java/com/generation/cacique/cacique/model/Produto.java
+++ src/main/java/com/generation/cacique/cacique/model/Produto.java
@@ -30,4 +30,51 @@ public class Produto {
 
     private String foto;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Long quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
